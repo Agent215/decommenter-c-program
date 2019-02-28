f1f49602615b74d7ac60aaa398a321e7cc727b64
Brahm <abraham.schultz@gmail.com>
2019-02-28 11:02:48 -0500

    Update rmcmt.c
    Added some documentation

--- rmcmt.c
+++ rmcmt.c
@@ -2,17 +2,17 @@
  /* Abraham Schultz */
  /* Last edited 2/19/2019 */
  /* This is a c program that removes comments from a c file
-    I got a lot of help from a program found online written by Mike Sullivan
+    I got a lot of help from a program found at https://www.msully.net/files/decomment.c written by Mike Sullivan
  */
 #include <stdio.h>
-#include <string.h> 
+#include <string.h>
 #include <errno.h>
 #include <stdlib.h>
 
 #define BUF_SIZE 64
+#define OUT 0
 
-
-/* function declerations */
+/* function declaration  */
 int decomment(FILE *f);
 int lineCounter = 0;
 
@@ -21,9 +21,9 @@ int main(int argc, char *argv[]) {
 	FILE* f;
 	// create array for input string
         char s[BUF_SIZE];
-	// take input from keyboard 
+	// take input from keyboard
 	scanf("%s", s);
-	// open file, f = NULL if can not open file stream  
+	// open file, f = NULL if can not open file stream
 	 f  = fopen (s, "rw");
 
 	//check if file is open
@@ -32,8 +32,8 @@ int main(int argc, char *argv[]) {
 	return EXIT_FAILURE;
        }
          else
-	
-	// call function to decomment file 
+
+	// call function to de-comment file
 	decomment(f);
 	// close file stream
 	fclose(f);
@@ -45,25 +45,25 @@ int decomment(FILE *f) {
 	int in_comment; /* whether it is currently inside a comment */
 	int in_quote; /* whether it is currently inside quotes */
 	char quote_type; /* whether it's a double or single quote */
-	int c, tmp;   
+	int c, tmp;
 	/* sets all states to 0 on first pass */
-	c = escape = quote_type = in_quote = in_comment = 0;
-	while ((c = getc(f)) != EOF) {	
+	c = escape = quote_type = in_quote = in_comment = OUT;
+	while ((c = getc(f)) != EOF) {
 		/* count lines */
 		if (c == '\n') {
 		lineCounter ++ ;
-                }	
-		/* check for begining of c comment*/
+                }
+		/* check for beginning  of c comment*/
 		if (!in_quote && c == '/') {
 			if ((tmp = getc(f)) == '*')
 				in_comment = 1;
 			else
 				ungetc(tmp, f);
 		}
-		/* checks for end of c commennt */
+		/* checks for end of c comment  */
 		if (in_comment && c == '*') {
 			if ((tmp = getc(f)) == '/') {
-				in_comment = 0;
+				in_comment = OUT;
 				continue;
 			} else
 				ungetc(tmp, f);
@@ -71,27 +71,28 @@ int decomment(FILE *f) {
 
 		/* check to see if we are inside a quote   */
 		if (in_quote && !escape && c == quote_type)
-			quote_type = in_quote = 0;
+			quote_type = in_quote = OUT;
 		else if (!in_quote && !in_comment && (c == '\'' || c == '"')) {
 			in_quote = 1;
 			quote_type = c;
 		}
+		/* check to see if this is an escape sequence in a quote */
 		if (in_quote && c == '\\' && !escape)
 			escape = 1;
 		else
-			escape = 0;
-		
-		/* if we are not in a comment then display the current char*/ 
+			escape = OUT;
+
+		/* if we are not in a comment then display the current char*/
 		if (!in_comment)
 			putchar(c);
 	} // end while
 
-	/*  after while loop we should not be in a comment, but we check anyway  */ 	
+	/*  after while loop we should not be in a comment, but we check anyway  */
 	if (in_comment) {
         /* checks to see if incomplete comment  */
                  printf ("\nERROR: Incomplete comment at line %d \n", lineCounter);
-                
 
-	}	
+
+	}
 	return 0;
-} // end decomment 
+} // end de-comment
